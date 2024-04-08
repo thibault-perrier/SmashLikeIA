4f295c022332080aca514428de1be1c393e4bb7d
AlexisVildy <alexisvildy@gmail.com>
2024-04-08 10:34:40 +0200

    a

--- Assets/IASmashAgent.cs
+++ Assets/IASmashAgent.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 
-public class IASmashAgent : Agent
+public class BasicAgents : MonoBehaviour
 {
     public override void Initialize()
     {
-
+        //Reset the agent when a episode begins
     }
 
     public override void OnEpisodeBegin()
     {
+        //Reset environnement
+        //Reset moves, agents
+    }
 
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        //interpret vectorAction in Unity
+    }
+
+    public override void CollectsObservations(VectorSensor sensor)
+    {
+        //Reset environnement
+        //Reset moves, agents
     }
 }
